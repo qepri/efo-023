a54538699425c66f3aeb3f5843dffae58f1f7099
qepri.xyz <qepri.xyz@gmail.com>
2023-09-30 15:16:18 -0600

    updated app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,8 @@
-
 async function fetchJSON(url) {
     try {
         const response = await fetch(url);
         if (!response.ok) throw new Error('Network response was not ok' + response.statusText);
-        const jsonData = JSON.parse(await response.text());
+        const jsonData = await response.json(); // directly parse JSON
         createTable(jsonData);
     } catch (error) {
         console.error('Error fetching or parsing JSON', error);
@@ -12,20 +11,32 @@ async function fetchJSON(url) {
 
 function createTable(data) {
     const container = document.getElementById('table-container');
+    if(!container) {
+        console.error('Container element not found');
+        return;
+    }
+    
     const table = document.createElement('table');
     table.className = 'table table-bordered';
     
+    // Create Table Head
     const thead = document.createElement('thead');
     const headerRow = document.createElement('tr');
-    for(const key in data[0]) {
-        const th = document.createElement('th');
-        th.textContent = key;
-        headerRow.appendChild(th);
+    
+    // Assuming data is an array and has at least one object
+    if(data.length && typeof data[0] === 'object') {
+        for(const key in data[0]) {
+            const th = document.createElement('th');
+            th.textContent = key;
+            headerRow.appendChild(th);
+        }
     }
     thead.appendChild(headerRow);
     table.appendChild(thead);
     
+    // Create Table Body
     const tbody = document.createElement('tbody');
+    
     data.forEach(row => {
         const tr = document.createElement('tr');
         for(const key in row) {
@@ -35,21 +46,11 @@ function createTable(data) {
         }
         tbody.appendChild(tr);
     });
+    
     table.appendChild(tbody);
     
     container.appendChild(table);
 }
 
-
-
-
 // Call the fetchJSON function with the URL of your JSON file
 fetchJSON('assets/js/EFO023.json');
-
-
-
-
-
-
-
-
